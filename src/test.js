aef1b4a7c45da714da394d51fd93f1475d039fa1
Patrick_B <pb5191@gmail.com>
2020-02-25 16:16:52 -0500

    added some dates to test fiscal year calculation

---
+++ src/test.js
@@ -0,0 +1,4 @@
+const today = new Date();
+const dates = ["08/12/2019", "01/15/2020", "03/15/2019"];
+const newFiscalYear = new Date("07/01/" + today.getFullYear());
+console.log("newFiscalYear :", newFiscalYear);

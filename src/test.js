d24982ecb24a853250afef72b0226f5a1fd4f279
Patrick_B <pb5191@gmail.com>
2020-03-03 21:02:55 -0500

    testing web worker

--- src/test.js
+++ src/test.js
@@ -1,4 +1,1 @@
-const today = new Date();
-const dates = ["08/12/2019", "01/15/2020", "03/15/2019"];
-const newFiscalYear = new Date("07/01/" + today.getFullYear());
-console.log("newFiscalYear :", newFiscalYear);
+const worker = new Worker("web-worker.js");

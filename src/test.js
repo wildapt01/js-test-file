10af4e325a80c1f4fe678fa4729aab10885ce665
Patrick_B <pb5191@gmail.com>
2020-03-30 12:43:27 -0400

    validate hashtable values

--- src/test.js
+++ src/test.js
@@ -1,1 +1,47 @@
-const worker = new Worker("web-worker.js");
+// Test for Validation of Data
+
+const incoming = [
+  { name: "AgencyID", value: "PRA" },
+  { name: "FSID", value: "L" },
+  { name: "Amount", value: "as5000" },
+  { name: "FundStart", value: "07/01/2019" },
+  { name: "FundEnd", value: "06/30/2020" },
+  { name: "FundNumber", value: "Ax-23-45-FO" },
+  { name: "Purpose", value: "Something usefusl" }
+];
+
+const validTest = list => {
+  const result = [];
+  for (let obj of list) {
+    switch (obj.name) {
+      case "Amount":
+        if (obj.value) {
+          obj.correct = Number(obj.value) ? true : false;
+        } else {
+          obj.correct = true;
+        }
+        break;
+      case "FundStart":
+        obj.correct = moment(obj.value, "MM/DD/YYYY", true).isValid();
+        break;
+
+      case "FundEnd":
+        obj.correct = moment(obj.value, "MM/DD/YYYY", true).isValid();
+        break;
+
+      default:
+        obj.correct = true;
+        break;
+    }
+    result.push(obj);
+  }
+  return result;
+};
+
+const validatedObject = validTest(incoming);
+const checkFlag = validatedObject.some(item => !item.correct);
+console.log("checkFlag :", checkFlag);
+if (checkFlag) {
+  document.querySelector("h2").style.color = "red";
+}
+document.body.append(`<div>${JSON.stringify(validatedObject)}</div>`);

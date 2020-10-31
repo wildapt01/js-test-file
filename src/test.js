74aa98a399e78db5ea3d8906556a3dac71d0c185
Patrick_B <pb5191@gmail.com>
2020-10-31 15:08:25 -0400

    done

--- src/test.js
+++ src/test.js
@@ -1,35 +1,29 @@
 //* Date Validation
 
-const dateValidation = (strDate) => {
-  const match = strDate.match(/^(\d\d)\/(\d\d)\/(\d{4})$/) || [];
-  const month = (match[1] | 0) - 1;
-  const day = match[2] | 0;
-  const year = match[3] | 0;
-  // console.log("date: ", strDate);
-  // console.log(
-  //   month === 1 &&
-  //     !(day < 30) &&
-  //     Boolean((!(year % 4) && year % 100) || !(year % 400))
-  // );
-
+function dateValid(date) {
+  var match = date.match(/^(\d\d)\/(\d\d)\/(\d{4})$/) || [];
+  var m = (match[1] | 0) - 1;
+  var d = match[2] | 0;
+  var y = match[3] | 0;
   return !(
-    month < 0 || // Before January
-    month > 11 || // After December
-    day < 1 || // Before the 1st of the month
-    day - 30 > ((2773 >> month) & 1) || // After the 30th or 31st of the month using bitmap
-    !(
-      month === 1 &&
-      day < 30 &&
-      Boolean((!(year % 4) && year % 100) || !(year % 400))
-    )
+    m < 0 || // Before January
+    m > 11 || // After December
+    d < 1 || // Before the 1st of the month
+    d - 30 > ((2773 >> m) & 1) || // After the 30th or 31st of the month using bitmap
+    (m == 1 &&
+      // d - 28 > ((!(y % 4) && y % 100) || !(y % 400)))
+      d - 28 > !(y % 4 || (!(y % 100) && y % 400)))
   );
-};
+}
 
-console.log("01/01/2020 ==> ", dateValidation("01/01/2020"));
-console.log("13/01/2020 ==> ", dateValidation("13/01/2020"));
-console.log("02/30/2020 ==> ", dateValidation("02/30/2020"));
-console.log("02/29/2020 ==> ", dateValidation("02/29/2020"));
-console.log("02/29/2021 ==> ", dateValidation("02/29/2021"));
-console.log("02/29/2001 ==> ", dateValidation("02/29/2001"));
-console.log("02/28/2001 ==> ", dateValidation("02/28/2001"));
-console.log("02/29/2000 ==> ", dateValidation("02/29/2000"));
+console.log("01/01/2020 ==> ", dateValid("01/01/2020"));
+console.log("13/01/2020 ==> ", dateValid("13/01/2020"));
+console.log("02/29/2000 ==> ", dateValid("02/29/2000"));
+console.log("02/29/2001 ==> ", dateValid("02/29/2001"));
+console.log("02/30/2020 ==> ", dateValid("02/30/2020"));
+console.log("02/29/2020 ==> ", dateValid("02/29/2020"));
+console.log("02/29/2021 ==> ", dateValid("02/29/2021"));
+console.log("02/29/2001 ==> ", dateValid("02/29/2001"));
+console.log("02/27/2001 ==> ", dateValid("02/27/2001"));
+console.log("02/29/2000 ==> ", dateValid("02/29/2000"));
+console.log("02/30/2000 ==> ", dateValid("02/29/2000"));

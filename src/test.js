06099a587c9d62e50088dbf941ab9347a67df23c
Patrick_B <pb5191@gmail.com>
2020-11-02 08:41:23 -0500

    date validation that works

--- src/test.js
+++ src/test.js
@@ -1,29 +1,45 @@
 //* Date Validation
 
-function dateValid(date) {
-  var match = date.match(/^(\d\d)\/(\d\d)\/(\d{4})$/) || [];
-  var m = (match[1] | 0) - 1;
-  var d = match[2] | 0;
-  var y = match[3] | 0;
-  return !(
-    m < 0 || // Before January
-    m > 11 || // After December
-    d < 1 || // Before the 1st of the month
-    d - 30 > ((2773 >> m) & 1) || // After the 30th or 31st of the month using bitmap
-    (m == 1 &&
-      // d - 28 > ((!(y % 4) && y % 100) || !(y % 400)))
-      d - 28 > !(y % 4 || (!(y % 100) && y % 400)))
-  );
-}
+const dateValid = (date) => {
+  const isLeapYear = (yearNum) => {
+    return (yearNum % 100 === 0 ? yearNum % 400 === 0 : yearNum % 4 === 0)
+      ? 1
+      : 0;
+  };
+  const match = date.match(/^(\d\d)\/(\d\d)\/(\d{4})$/) || [];
+  const month = (match[1] | 0) - 1;
+  const day = match[2] | 0;
+  const year = match[3] | 0;
 
-console.log("01/01/2020 ==> ", dateValid("01/01/2020"));
-console.log("13/01/2020 ==> ", dateValid("13/01/2020"));
-console.log("02/29/2000 ==> ", dateValid("02/29/2000"));
-console.log("02/29/2001 ==> ", dateValid("02/29/2001"));
-console.log("02/30/2020 ==> ", dateValid("02/30/2020"));
-console.log("02/29/2020 ==> ", dateValid("02/29/2020"));
-console.log("02/29/2021 ==> ", dateValid("02/29/2021"));
-console.log("02/29/2001 ==> ", dateValid("02/29/2001"));
-console.log("02/27/2001 ==> ", dateValid("02/27/2001"));
-console.log("02/29/2000 ==> ", dateValid("02/29/2000"));
-console.log("02/30/2000 ==> ", dateValid("02/29/2000"));
+  const dateEval = !(
+    (
+      month < 0 || // Before January
+      month > 11 || // After December
+      day < 1 || // Before the 1st of the month
+      day - 30 > ((2773 >> month) & 1) ||
+      (month === 1 && day - 28 > isLeapYear(year))
+    )
+    // Day is 28 or 29, month is 02, year is leap year ==> true
+  );
+
+  return `\nDate: ${date}\n\n     Valid Date?: ${dateEval}\n=======================================`;
+};
+
+console.log(dateValid("02/28/2020")); // true
+console.log(dateValid("02/29/2020")); // true
+console.log(dateValid("02/30/2020")); // false
+console.log(dateValid("01/31/2020")); // true
+console.log(dateValid("01/31/2000")); // true
+console.log(dateValid("04/31/2020")); // false
+console.log(dateValid("04/31/2000")); // false
+console.log(dateValid("04/30/2020")); // true
+console.log(dateValid("01/32/2020")); // false
+console.log(dateValid("02/28/2021")); // true
+console.log(dateValid("02/29/2021")); // false
+console.log(dateValid("02/30/2021")); // false
+console.log(dateValid("02/28/2000")); // true
+console.log(dateValid("02/29/2000")); // true
+console.log(dateValid("02/30/2000")); // false
+console.log(dateValid("02/28/2001")); // true
+console.log(dateValid("02/29/2001")); // false
+console.log(dateValid("02/30/2001")); // false

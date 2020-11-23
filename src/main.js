43573e7260b658ed54137104cc0b36048ecc31c8
Patrick_B <pb5191@gmail.com>
2020-11-22 20:42:10 -0500

    added redirecting feature

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@
 
 $(document).ready(() => {
   // Button action
-  const entry = $(".input-zone").val();
 
-  $(".btn").click((evnt) => {
+  $(".input-zone").change((evnt) => {
     evnt.preventDefault();
+    const entry = $(".input-zone").val();
+    console.log("entry :>> ", entry);
+    if (entry) {
+      const newURL = `../src/second.html/?val=${entry}`;
+      $(".link").attr("href", newURL);
+    }
   });
 });

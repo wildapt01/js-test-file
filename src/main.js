27533f1434d3a78c8fb82ae2e7be9915c7bf9ec3
Patrick_B <pb5191@gmail.com>
2021-05-02 12:40:04 -0400

    added some variations

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import { adding } from "./adding.js";
 //* jQuery section
 
 $(document).ready(() => {
-  QUnit.module("add", function () {
-    QUnit.test("should add two numbers", function (assert) {
+  QUnit.module("add", () => {
+    QUnit.test("should add two numbers", (assert) => {
       assert.equal(adding(1, 1), 2);
-      assert.equal(adding(1, "1"), 2);
+      assert.equal(adding(1, "1"), 2, "Failing test as 1 param is a string");
+    });
+    QUnit.test("should not add a number and a string", (assert) => {
       assert.strictEqual(adding(1, "1"), "11");
       assert.true(typeof adding(1, "1") === "number");
-      assert.true(typeof adding(1, 1) === "number");
+      assert.true(typeof adding(1, 1) === "number", "Should be true!");
     });
   });
 });

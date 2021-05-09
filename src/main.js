b9720f1452b77fcda42be68b67bd9f0427d27183
Patrick_B <pb5191@gmail.com>
2021-05-09 07:58:49 -0400

    added no collapse flag

--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import { adding } from "./adding.js";
 
 $(document).ready(() => {
   QUnit.module("add", () => {
+    QUnit.config.collapse = false;
     QUnit.test("should add two numbers", (assert) => {
       assert.equal(adding(1, 1), 2);
-      assert.equal(adding(1, "1"), 2, "Failing test as 1 param is a string");
+      assert.equal(adding(1, "1"), "11", "Failing test as 1 param is a string");
     });
     QUnit.test("should not add a number and a string", (assert) => {
       assert.strictEqual(adding(1, "1"), "11");
-      assert.true(typeof adding(1, "1") === "number");
+      assert.true(typeof adding(1, "1") === "string");
       assert.true(typeof adding(1, 1) === "number", "Should be true!");
     });
   });

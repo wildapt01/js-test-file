2020eee8edc9fcf86494192539d4d4a0691af752
Patrick_B <pb5191@gmail.com>
2021-09-02 13:08:06 -0400

    testing URL API

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 //* Logic
-import { adding } from "./adding.js";
+
+
+
 
 //* jQuery section
 
 $(document).ready(() => {
-  QUnit.module("add", () => {
-    QUnit.config.collapse = false;
-    QUnit.test("should add two numbers", (assert) => {
-      assert.equal(adding(1, 1), 2);
-      assert.equal(adding(1, "1"), "11", "Failing test as 1 param is a string");
-    });
-    QUnit.test("should not add a number and a string", (assert) => {
-      assert.strictEqual(adding(1, "1"), "11");
-      assert.true(typeof adding(1, "1") === "string");
-      assert.true(typeof adding(1, 1) === "number", "Should be true!");
-    });
-  });
+const currentURL=window.location.href
+$("#myURL").text(currentURL)
+
+$("#next-page-btn").click((evnt)=>{
+  evnt.stopPropagation()
+  const newURL="http://localhost:5500/next_page.html"
+  const queryParam="?st_id=0001"
+
+
+  window.location.assign(`${newURL}${queryParam}`)
+})
 });
